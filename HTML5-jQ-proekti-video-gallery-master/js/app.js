db145f3bfb004bc6a327bb5cb6998932d23fc035
Kirco <stoimenov.kircho@gmail.com>
2020-10-07 15:53:24 +0200

    s

---
+++ HTML5-jQ-proekti-video-gallery-master/js/app.js
@@ -0,0 +1,19 @@
+$().ready(function () {
+
+    $('.video-Gallery-Wrap').on('click', 'img', function () {
+
+        const videoName = `video/${$(this).attr('alt')}.mp4`;
+        const videoElement = $(`<div class='video-container'><video src='${videoName}' controls width="800" height="450" ></video><img src='times-circle-regular.svg' class='close'></div>`);
+
+        $('body').prepend(videoElement)
+
+        console.log(videoName)
+        $('.close').on('click', function () {
+            $('.video-container').remove()
+
+
+        })
+    })
+
+
+})
