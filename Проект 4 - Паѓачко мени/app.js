763a0402b238c7a6977f05ecc1f9882fa374cf43
Kirco <stoimenov.kircho@gmail.com>
2020-12-02 09:48:53 +0100

    zadaca 4

---
+++ Проект 4 - Паѓачко мени/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#menu li").hover(function(){
+      $(".dropdown-menu", this).slideDown(100);
+    }, function(){
+      $(".dropdown-menu", this).stop().slideUp(100);
+    });
+  })
